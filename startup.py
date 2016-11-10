8d44999144f75959946da059158e1fcf17c338d5
johnwheeler <john@johnwheeler.org>
2016-11-09 20:44:21 -0800

    put files back into root of directory. create source directories and destination directories

--- startup.py
+++ startup.py
@@ -1,3 +1,6 @@
+import os
+import shutil
+
 import click
 
 
@@ -7,8 +10,24 @@ def cli():
 
 
 @cli.command()
-def start():
-    print("Hello, World")
+@click.argument('project_name')
+def start(project_name):
+    # create the source directory paths
+    src_dir = os.path.dirname(os.path.realpath(__file__))
+    src_app_dir = os.path.join(src_dir, 'app')
+    src_etc_dir = os.path.join(src_dir, 'etc')
+
+    # create the destination directory paths
+    dest_dir = project_name
+    dest_app_dir = os.path.join(dest_dir, project_name)
+    dest_etc_dir = os.path.join(dest_dir, 'etc')
+
+    # make the destination directory
+    os.mkdir(dest_dir)
+
+    # copy the app directory into project_name
+    shutil.copytree(src_app_dir, dest_app_dir)
+    shutil.copytree(src_etc_dir, dest_etc_dir)
 
 
 if __name__ == '__main__':
