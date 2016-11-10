9b72dcace2dbb6737fc2ed8b40b9bd2a8c6503e2
johnwheeler <john@johnwheeler.org>
2016-11-09 20:51:36 -0800

    much shorter path to create project

--- startup.py
+++ startup.py
@@ -12,22 +12,12 @@ def cli():
 @cli.command()
 @click.argument('project_name')
 def start(project_name):
-    # create the source directory paths
-    src_dir = os.path.dirname(os.path.realpath(__file__))
-    src_app_dir = os.path.join(src_dir, 'app')
-    src_etc_dir = os.path.join(src_dir, 'etc')
-
-    # create the destination directory paths
-    dest_dir = project_name
-    dest_app_dir = os.path.join(dest_dir, project_name)
-    dest_etc_dir = os.path.join(dest_dir, 'etc')
-
-    # make the destination directory
-    os.mkdir(dest_dir)
-
-    # copy the app directory into project_name
-    shutil.copytree(src_app_dir, dest_app_dir)
-    shutil.copytree(src_etc_dir, dest_etc_dir)
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    src_dir = os.path.join(cwd, 'src')
+    shutil.copytree(src_dir, project_name)
+    old_py = os.path.join(project_name, 'app')
+    new_py = os.path.join(project_name, project_name)
+    os.rename(old_py, new_py)
 
 
 if __name__ == '__main__':
