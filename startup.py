74d1002ddebafde7d478bc1e91467ebbad018e39
johnwheeler <john@johnwheeler.org>
2016-11-10 20:00:34 -0800

    added template_files

--- startup.py
+++ startup.py
@@ -11,13 +11,23 @@ def cli():
 
 @cli.command()
 @click.argument('project_name')
-def start(project_name):
+def new(project_name):
     cwd = os.path.dirname(os.path.realpath(__file__))
-    src_dir = os.path.join(cwd, 'src')
-    shutil.copytree(src_dir, project_name)
-    old_py = os.path.join(project_name, 'app')
-    new_py = os.path.join(project_name, project_name)
-    os.rename(old_py, new_py)
+
+    # copy the project template dir
+    project_template_dir = os.path.join(cwd, 'template_files/project')
+    shutil.copytree(project_template_dir, project_name)
+
+    # rename the app to project_name
+    old_app = os.path.join(project_name, 'app')
+    new_app = os.path.join(project_name, project_name)
+    os.rename(old_app, new_app)
+
+    # show command feedback
+    for root, dirs, files in os.walk(project_name):
+        for f in files:
+            print root + os.sep + f
+    click.echo('created {}'.format(project_name))
 
 
 if __name__ == '__main__':
--- template_files/project/app/views.py
+++ template_files/project/app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 
-from app import app
+from . import app
 
 
 @app.route('/')
