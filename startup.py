a6c238c336630346aa94e10cfd0e63dce957a929
johnwheeler <john@johnwheeler.org>
2016-11-10 20:42:09 -0800

    rearrange files. write settings file with jinja templates

--- startup.py
+++ startup.py
@@ -1,7 +1,15 @@
 import os
 import shutil
+from binascii import b2a_hex
+from textwrap import dedent
 
 import click
+from jinja2 import Environment, FileSystemLoader
+
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+template_dir = os.path.join(CWD, 'templates')
+jinja_env = Environment(loader=FileSystemLoader(template_dir))
 
 
 @click.group()
@@ -12,16 +20,16 @@ def cli():
 @cli.command()
 @click.argument('project_name')
 def new(project_name):
-    cwd = os.path.dirname(os.path.realpath(__file__))
-
     # copy the project template dir
-    project_template_dir = os.path.join(cwd, 'template_files/project')
+    project_template_dir = os.path.join(CWD, 'project')
     shutil.copytree(project_template_dir, project_name)
 
     # rename the app to project_name
-    old_app = os.path.join(project_name, 'app')
-    new_app = os.path.join(project_name, project_name)
-    os.rename(old_app, new_app)
+    old_app_dir = os.path.join(project_name, 'app')
+    app_dir = os.path.join(project_name, project_name)
+    os.rename(old_app_dir, app_dir)
+
+    _write_project_settings(app_dir, project_name)
 
     # show command feedback
     for root, dirs, files in os.walk(project_name):
@@ -30,5 +38,13 @@ def new(project_name):
     click.echo('created {}'.format(project_name))
 
 
-if __name__ == '__main__':
-    cli()
+def _write_project_settings(app_dir, project_name):
+    settings_file = os.path.join(app_dir, 'settings.cfg')
+
+    secure_random = b2a_hex(os.urandom(24))
+    template = jinja_env.get_template('settings.cfg.tpl')
+    file_contents = template.render(secure_random=secure_random,
+                                    project_name=project_name)
+
+    with open(settings_file, 'w') as f:
+        f.write(file_contents)
---
+++ templates/settings.cfg.tpl
@@ -0,0 +1,4 @@
+SECRET_KEY = "{{ secure_random }}"
+
+SQLALCHEMY_DATABASE_URI = "postgres://{{ project_name }}"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
