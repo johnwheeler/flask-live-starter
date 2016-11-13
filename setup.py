f092b21889a300771d27f16f1a8dd0a92210a619
johnwheeler <john@johnwheeler.org>
2016-11-12 18:22:11 -0800

    added italian press

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='startup-starter',
+    name='mvp-starter',
     version='0.0',
-    py_modules='startup.py',
+    py_modules='mvp.py',
     install_requires=reqs,
     entry_points='''
         [console_scripts]
-        startup=startup:cli
+        mvp=mvp:cli
     '''
 )
