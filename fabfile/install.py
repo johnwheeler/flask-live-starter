d7a1ebeaeec5196f63073fdb3e9d23736034872f
johnwheeler <john@johnwheeler.org>
2016-12-11 22:20:25 -0800

    redis install task

--- fabfile/install.py
+++ fabfile/install.py
@@ -1,9 +1,11 @@
 from fabric.api import task, sudo
+from fabric.contrib.files import sed
+from fabric.context_managers import cd
 
 from .constants import *
 
 
-__all__ = ['system', 'certificate', 'postgres']
+__all__ = ['system', 'certificate', 'postgres', 'redis']
 
 
 @task
@@ -28,10 +30,12 @@ def system():
 
 @task
 def postgres():
+    # add postgres repository to apt
     sudo("sh -c 'echo deb http://apt.postgresql.org/pub/repos/apt/ jessie-pgdg main > /etc/apt/sources.list.d/pgdg.list'")
     sudo('wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -')
     sudo('apt-get update')
 
+    # install postgresql components
     pg_version = '9.6'
     _install('postgresql-{}'.format(pg_version))
     _install('postgresql-client-{}'.format(pg_version))
@@ -40,9 +44,48 @@ def postgres():
 
 
 @task
+def redis():
+    # make required directories
+    sudo('mkdir /etc/redis')
+    sudo('mkdir /var/redis')
+    sudo('mkdir /var/redis/6379')
+
+    # download
+    sudo('wget http://download.redis.io/redis-stable.tar.gz')
+    # untar
+    sudo('tar xzf redis-stable.tar.gz')
+
+    with cd('redis-stable'):
+        # make & make install
+        sudo('make')
+        sudo('make install')
+        # copy system init script
+        sudo('cp utils/redis_init_script /etc/init.d/redis_6379')
+        # copy redis configuration file
+        sudo('cp redis.conf /etc/redis/6379.conf')
+
+    # edit the configuration file
+    sed('/etc/redis/6379.conf', '^daemonize no$', 'daemonize yes', use_sudo=True)
+    sed('/etc/redis/6379.conf', '^logfile ""$',
+        'logfile /var/log/redis_6379.log', use_sudo=True)
+    sed('/etc/redis/6379.conf', '^dir ./$',
+        'dir /var/redis/6379', use_sudo=True)
+
+    # # update script init links
+    sudo('update-rc.d redis_6379 defaults')
+    # start redis system service
+    sudo('service redis_6379 start')
+
+    # clean up
+    sudo('rm -rf redis-stable')
+    sudo('rm redis-stable.tar.gz')
+
+
+@task
 def certificate():
     _install('python-certbot-nginx -t jessie-backports')
-    sudo('certbot --nginx --non-interactive --agree-tos --redirect --domain {} --domain {}.{} --email {}'.format(DOMAIN, SUBDOMAIN, DOMAIN, EMAIL))
+    sudo('certbot --nginx --non-interactive --agree-tos --redirect --domain {} --domain {}.{} --email {}'
+         .format(DOMAIN, SUBDOMAIN, DOMAIN, EMAIL))
 
 
 def _system_update_upgrade():
